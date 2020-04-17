e77268a87a71084570982b64dae40227ca83edd4
Mark Rousskov <mark.simulacrum@gmail.com>
2020-04-17 12:07:34 -0400

    Utilize triagebot-based API
    This does not need authentication, and is a simpler and faster API (due to
    needing just the one request).

--- examples/sample.rs
+++ examples/sample.rs
@@ -1,11 +1,7 @@
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let client = reqwest::Client::new();
-    let list =
-        rustc_artifacts::master_commits(&client, std::env::var("GITHUB_API_TOKEN").ok().as_deref())
-            .await
-            .unwrap();
+    let list = rustc_artifacts::master_commits().await.unwrap();
     eprintln!("first: {:#?}", list.first());
     eprintln!("last: {:#?}", list.last());
     eprintln!("commits: {}", list.len());
